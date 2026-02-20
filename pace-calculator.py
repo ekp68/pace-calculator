3d668c561954129649327f00faf8d1f3773eaa8d
Math1029 <haastiin@protonmail.com>
2026-02-20 13:59:43 -0700

    Complete most of the code for calculating the pace and speed

--- pace-calculator.py
+++ pace-calculator.py
@@ -1,9 +1,29 @@
 def main():
     kilometers = float(input("Enter distance (km): "))
     minutes, seconds = map(int, input("Enter time (min sec): ").split())
+    seconds += 60*minutes
 
-    print(f"Distance {kilometers}")
-    print(f"Time {minutes} min {seconds} sec")
+    hours = seconds / 3600
+    meters = 1000*kilometers
+    miles = kilometers / 1.609344
+
+    time_km = seconds / kilometers
+    minute_km = int(time_km / 60)
+    second_km = int(time_km) % 60
+
+    time_mi = seconds / kilometers
+    minute_mi = int(time_mi / 60)
+    second_mi = int(time_mi) % 60
+
+    speed_kph = kilometers / hours
+    speed_mps = meters / seconds
+    speed_mph = miles / hours
+
+    print(f"Pace (km): {minute_km} minutes {second_km} seconds")
+    print(f"Pace (mi): {minute_mi} minutes {second_mi} seconds")
+    print(f"Speed (kph): {speed_kph}")
+    print(f"Speed (m/s): {speed_mps}")
+    print(f"Speed (mph): {speed_mph}")
 
 if __name__ == "__main__":
     main()
